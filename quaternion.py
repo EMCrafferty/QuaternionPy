7477c3c61e7f8953b23adf6fd5a63cb1ca896ccc
Eoin Crafferty <EMCrafferty@gmail.com>
2021-05-11 18:55:42 -0400

    Normalization

--- quaternion.py
+++ quaternion.py
@@ -1,3 +1,4 @@
+import math
 from functools import reduce
 
 
@@ -11,9 +12,21 @@ class ImaginaryValue:
         else:
             raise TypeError
 
+    def __truediv__(self, other):
+        if isinstance(other, (float, int)):
+            return self.value / other
+
+    def __sub__(self, other):
+        return self.value - other.value
+
     def __neg__(self):
         return self.__class__(-self.value)
 
+    def __float__(self):
+        return self.value
+
+    def __repr__(self):
+        return str(self)
 
 class IVec(ImaginaryValue):
     def __init__(self, value: float):
@@ -36,7 +49,7 @@ class IVec(ImaginaryValue):
             raise TypeError
 
     def __str__(self):
-        return f"{self.value}i"
+        return f"{self.value:.3f}i"
 
 
 class JVec(ImaginaryValue):
@@ -60,7 +73,8 @@ class JVec(ImaginaryValue):
             raise TypeError
 
     def __str__(self):
-        return f"{self.value}j"
+        return f"{self.value:.3f}j"
+
 
 
 class KVec(ImaginaryValue):
@@ -84,11 +98,11 @@ class KVec(ImaginaryValue):
             raise TypeError
 
     def __str__(self):
-        return f"{self.value}k"
+        return f"{self.value:.3f}k"
 
 
 class VectorPart:
-    def __init__(self, i: float, j: float, k: float):
+    def __init__(self, i, j, k):
         self.i = IVec(i)
         self.j = JVec(j)
         self.k = KVec(k)
@@ -98,23 +112,78 @@ class VectorPart:
         yield self.j.value
         yield self.k.value
 
+    def __repr__(self):
+        return f"{{ {self.i}, {self.j}, {self.k} }}"
+
+    def __mul__(self, other):
+        if not isinstance(other, self.__class__):
+            raise TypeError
+
+        return VectorPart(self.i * other.i,
+                          self.j * other.j,
+                          self.k * other.k)
+
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            return VectorPart(self.i / other,
+                              self.j / other,
+                              self.k / other)
+
     def __matmul__(self, other):
         if not isinstance(other, self.__class__):
             raise TypeError
 
         v_product = []
-        for v1, v2 in zip(self, other):
-            v_product.append(v1 * v2)
+        for vec1, vec2 in zip(self, other):
+            v_product.append(vec1 * vec2)
 
         return reduce(lambda x, y: x + y, v_product)
 
+    def __add__(self, other):
+        if isinstance(other, self.__class__):
+            return VectorPart(self.i + other.i,
+                              self.j + other.j,
+                              self.k + other.k)
 
+        elif isinstance(other, IVec):
+            return VectorPart(self.i + other.value,
+                              self.j,
+                              self.k)
+
+        elif isinstance(other, JVec):
+            return VectorPart(self.i,
+                              self.j + other.value,
+                              self.k)
+
+        elif isinstance(other, KVec):
+            return VectorPart(self.i,
+                              self.j,
+                              self.k + other.value)
+
+        else:
+            raise TypeError
 
 
 class Quaternion:
     def __init__(self, scalar_part: float, vector_part: VectorPart):
         self.scalar_part = scalar_part
         self.vector_part = vector_part
+
+    def __matmul__(self, other):
+        if not isinstance(other, self.__class__):
+            raise TypeError
+
+        return (self.vector_part @ other.vector_part) + self.scalar_part * other.scalar_part
+
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            return Quaternion(self.scalar_part / other, self.vector_part / other)
+
+    def __repr__(self):
+        return f"{self.scalar_part:.3f}, {self.vector_part}"
+
+    def normalize(self):
+        return self / math.sqrt(self @ self)
 
 
 if __name__ == '__main__':
@@ -146,6 +215,12 @@ if __name__ == '__main__':
     print("\nBinary Tests:")
     vector_part1 = VectorPart(1, 3, -5)
     vector_part2 = VectorPart(4, -2, -1)
+
+    print(f"Addition: {v1 + v1}")
+    print(f"Subtraction: {v1 - v1}")
+    print(f"Product: {vector_part1 * vector_part2}")
     print(f"Dot Product: {vector_part1 @ vector_part2}")
 
-
+    q1 = Quaternion(3, vector_part1)
+    print(q1)
+    print(q1.normalize())
